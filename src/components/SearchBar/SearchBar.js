4facd49bf1f45b05b7ab2d2c3654c9e55c79f774
Alison Lee <alison2686@gmail.com>
2022-05-24 22:12:40 -0700

    update App

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -7,32 +7,30 @@ const sortByOptions = {
     'Most Reviewed': 'review_count'
 }
 
-class SearchBar extends React.Componet {
-    renderSortByOptions() {
-        return Object.keys(sortByOptions).map(sortByOption => {
+const SearchBar = () => {
+   const renderSortByOptions = () => {
+             Object.keys(sortByOptions).map(sortByOption => {
             let sortByOptionValue = sortByOptions[sortByOption];
             return <li key={sortByOptionValue}> {sortByOption} </li>;
         });
       }
 
-      render() {
-          return (
-            <div class="SearchBar">
-            <div class="SearchBar-sort-options">
-              <ul>
-                {this.renderSortByOptions}
-              </ul>
-            </div>
-            <div class="SearchBar-fields">
-              <input placeholder="Search Businesses" />
-              <input placeholder="Where?" />
-            </div>
-            <div class="SearchBar-submit">
-              <a>Let's Go</a>
-            </div>
-          </div>
-          )
-      }
+  return (
+    <div class="SearchBar">
+    <div class="SearchBar-sort-options">
+      <ul>
+        {renderSortByOptions}
+      </ul>
+    </div>
+    <div class="SearchBar-fields">
+      <input placeholder="Search Businesses" />
+      <input placeholder="Where?" />
+    </div>
+    <div class="SearchBar-submit">
+      <a>Let's Go</a>
+    </div>
+  </div>
+  )
 }
 
 export default SearchBar
